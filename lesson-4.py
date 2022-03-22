3e0ab99f802fc699ae19eee6d8aec2e769ac7241
DaFalconHunter <123alexmbale@gmail.com>
2022-03-22 11:50:10 +0000

    shopping tax

--- lesson-4.py
+++ lesson-4.py
@@ -1,7 +1,21 @@
-head_circum = int(input("Head circumference: "))
-if head_circum > 40:
-    print("Hat size is too big - large")
-elif head_circum < 25:
-    print("Hat size is too small - small")
-else:
-    print("This hat size is medium")
+"""
+input item price
+input quantity purchased
+total spend = item price * quantity purchased
+if total spend > 50 then
+    discount = 10% of total spend
+    print “you are eligible for a 10% discount at ” + discount
+    amount to pay = total spend - discount
+    print “this is the amount to pay: ” + amount to pay
+else
+    print amount to pay
+endif
+"""
+
+item_price = float(input('Item Price: '))
+quantity = int(input('Quantity: '))
+total = item_price * quantity
+if total > 50:
+    discount = total * 0.1
+    print("You are eligible for a 10% discount at " + discount)
+    amount_due 
