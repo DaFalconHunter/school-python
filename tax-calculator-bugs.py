63f941d71452b15ca83c75f432ef1cef7d05c18c
DaFalconHunter <123alexmbale@gmail.com>
2022-03-29 10:32:34 +0000

    Refactor -=

--- tax-calculator-bugs.py
+++ tax-calculator-bugs.py
@@ -8,7 +8,7 @@ if salary < 30000:
     tax = salary * 0.2
 elif salary >= 30000:
 #Salaries over 30000 are taxed at 40% for anything over 30000
-    salary = salary - 30000
+    salary -= 30000
     tax = salary * 0.4 + 6000 
 else:
 #Salaries over 150000 are taxed at 45% for anything over 150000
