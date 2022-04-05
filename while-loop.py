8d9e0fb29e5abfa2becedc72e90c1bc88d9f0838
DaFalconHunter <123alexmbale@gmail.com>
2022-04-05 10:22:38 +0000

    Comment the code

--- while-loop.py
+++ while-loop.py
@@ -1,6 +1,10 @@
+# Input the password:
 password = input("Enter password: ")
+
+# Check that while the password is not equal to the required password, ask the user to enter it again:
 while password != "password123":
     password = input("Enter password again: ")
 
+# Print success message
 print("Password Correct")
 print("You're logged in")
