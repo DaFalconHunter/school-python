10e90982641577b8009dcde89cc6e4549ca59918
DaFalconHunter <123alexmbale@gmail.com>
2022-04-05 10:38:32 +0000

    Refactor code

--- while-loop.py
+++ while-loop.py
@@ -1,10 +1,14 @@
 # Input the password:
 password = input("Enter password: ")
+count = 0
 
 # Check that while the password is not equal to the required password, ask the user to enter it again:
-while password != "password123":
-    password = input("Enter password again: ")
-
-# Print success message
-print("Password Correct")
-print("You're logged in")
+while count != 3:
+    count += 1
+    if password == "password123":
+        print("Welcome, you are logged in.")
+        print("It took you " + str(count) + " times to login. Next time remember your password.")
+        count = 3
+    else:
+        print("This was attempt number " + str(count))
+        password = input("Try again: ")
