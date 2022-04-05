8309005ddd7f223f6a02f1c6bb91997150ed1284
DaFalconHunter <123alexmbale@gmail.com>
2022-04-05 10:20:38 +0000

    Initial Commit

---
+++ lesson-6.py
@@ -0,0 +1,8 @@
+# Import random module
+import random
+
+# Find a random number between 1 and 6
+number = random.randint(1, 6)
+
+# Print the result
+print("Your random number is " + str(number))
---
+++ while-loop.py
@@ -0,0 +1,6 @@
+password = input("Enter password: ")
+while password != "password123":
+    password = input("Enter password again: ")
+
+print("Password Correct")
+print("You're logged in")
