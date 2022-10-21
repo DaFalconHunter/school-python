e2f01db39d6867a82396ada91a51505d7403764e
DaFalconHunter <123alexmbale@gmail.com>
2022-10-21 08:42:16 +0000

    feat: complete task

---
+++ python-yr-9-2022/lesson-4-maths/ext-task-1-rounding.py
@@ -0,0 +1,7 @@
+# Task 1 Turf Calculator (Modified with rounding)
+
+print("Welcome to Turf Calculator!")
+length = round(float(input("Please enter the length of the garden in metres: ")))
+width = round(float(input("Please enter the width of the garden in metres: ")))
+area = length * width
+print(f'You will need {area} square metres of turf.')
