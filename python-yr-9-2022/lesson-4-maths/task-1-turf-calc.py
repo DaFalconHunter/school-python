36fd340fbd9b01a7ad4d6fb7a84ebe3410d59f52
DaFalconHunter <123alexmbale@gmail.com>
2022-10-21 08:24:16 +0000

    feat: complete task

---
+++ python-yr-9-2022/lesson-4-maths/task-1-turf-calc.py
@@ -0,0 +1,7 @@
+# Task 1 Turf Calculator:
+
+print("Welcome to Turf Calculator!")
+length = float(input("Please enter the length of the garden in metres: "))
+width = float(input("Please enter the width of the garden in metres: "))
+area = length * width
+print("You will need " + str(area) + " square metres of turf.")
