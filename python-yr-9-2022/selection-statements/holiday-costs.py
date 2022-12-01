4d2cf27965ef435c9101ab3a88b856d17462cc80
DaFalconHunter <123alexmbale@gmail.com>
2022-12-01 11:44:43 +0000

    feat: create free child place logic

--- python-yr-9-2022/selection-statements/holiday-costs.py
+++ python-yr-9-2022/selection-statements/holiday-costs.py
@@ -9,12 +9,19 @@ destination = input("What is your destination? ")
 adult_flight_cost = int(input("How much does 1 adult flight cost? "))
 adult_accom_cost = int(input("How much does 1 adult accommodation cost? "))
 
-# b)
+# 1. b)
 # - Ask how many children and adults are travelling:
 num_children = int(input("How many children are travelling with you? "))
 num_adults = int(input("And how many adults? "))
 
-# c)
+# 2. a)
+# - Free child place
+n_child_free = num_children - 1
+if n_child_free != num_children:
+    free_child_bool = True
+else:
+    free_child_bool = False
+# 1. c)
 # - Calculate flight costs for the whole party:
 
 #   - Sum all adults flight costs:
@@ -23,12 +30,12 @@ total_adults_flight_cost = adult_flight_cost * num_adults
 #   - Set 1 child's flight cost:
 child_flight_cost = adult_flight_cost / 2
 #   - Sum all children flight costs:
-total_child_flight_cost = child_flight_cost * num_children
+total_child_flight_cost = child_flight_cost * n_child_free
 
 #   - Sum flights cost:
 total_flight_cost = round(total_adults_flight_cost + total_child_flight_cost, 2)
 
-# d)
+# 1. d)
 # - Calculate accommodation costs for the whole party:
 
 #   - Sum all adults accommodation costs:
@@ -37,16 +44,16 @@ total_adults_accom_cost = adult_accom_cost * num_adults
 #   - Set 1 child's accommodation cost:
 child_accom_cost = adult_accom_cost / 2
 #   - Sum all children accommodation costs:
-total_child_accom_cost = child_accom_cost * num_children
+total_child_accom_cost = child_accom_cost * n_child_free
 
 #   - Sum accommodation cost:
 total_acccom_cost = round(total_adults_accom_cost + total_child_accom_cost, 2)
 
-# e)
+# 1. e)
 # - Calculate total holiday cost:
 total_holiday_cost = total_adults_flight_cost + total_child_flight_cost + total_adults_accom_cost + total_child_accom_cost
 
-# f)
+# 1. f)
 # - Print summary of all holiday details: 
 #   - Country ✔
 #   - Destination ✔
@@ -64,12 +71,25 @@ Country you are visiting: {country}
 Destination you are headed to: {destination}
 No. of adults travelling: {num_adults}
 No. of children travelling: {num_children}
+
+Discounts & deductions:
+ - Free child place incl. (True/False): {free_child_bool}
+
 Cost of flights: £{total_flight_cost}
 Cost of accommodation: £{total_acccom_cost}
 Total holiday cost: £{total_holiday_cost}
 
 Enjoy your holiday!
 **************************************************
-""".format(country=country, destination=destination, num_adults=num_adults, num_children=num_children, total_flight_cost=total_flight_cost, total_acccom_cost=total_acccom_cost, total_holiday_cost=total_holiday_cost)
+""".format(
+    country=country,
+    destination=destination,
+    num_adults=num_adults,
+    num_children=num_children,
+    free_child_bool=free_child_bool,
+    total_flight_cost=total_flight_cost,
+    total_acccom_cost=total_acccom_cost,
+    total_holiday_cost=total_holiday_cost
+)
 
 print(holiday_summary)
