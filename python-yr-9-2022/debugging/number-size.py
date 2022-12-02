0e6416b42017df02d5900520b0a029e80cfd5d15
DaFalconHunter <123alexmbale@gmail.com>
2022-12-02 09:31:53 +0000

    feat: initial commit

---
+++ python-yr-9-2022/debugging/number-size.py
@@ -0,0 +1,6 @@
+num1 = input("Enter a number: ") 
+num2 = int(input("Enter another number: ")
+if num1 > nums:
+    print(num1, "is bigger than", num2)
+else:
+    print(num1, "is smaller than", num2)
