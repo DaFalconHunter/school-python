0feef0838546da46b024ebbe6706697f001bfc5f
DaFalconHunter <123alexmbale@gmail.com>
2022-12-05 20:07:18 +0000

    style: comment and format code

--- python-yr-9-2022/debugging/rock-paper-scissor.py
+++ python-yr-9-2022/debugging/rock-paper-scissor.py
@@ -1,20 +1,30 @@
+# Import random module:
 import random
-compchoice = random.choice(["R","P","S"])
+
+# Initialise computer's random choice:
+compchoice = random.choice(["R", "P", "S"])
+
+# Display selection menu:
 print("Pick one of the following")
 print()
 print("(R or r) Rock")
 print("(P or p) Paper")
 print("(S or s) Scissors")
+# Ask for user selection input:
 userchoice = input("Enter R, P or S or r, p, s alternatively: ")
 
+# Set play state to True:
 play = True
 
+# Display computer's random choice:
 if compchoice == "R":
     print("I selected...Rock")
 elif compchoice == "P":
     print("I selected...Paper")
 else:
     print("I selected...Scissors")
+
+# Display user selection choice:
 if userchoice == "R" or userchoice == "r":
     print("You selected...Rock")
 elif userchoice == "P" or userchoice == "p":
@@ -22,11 +32,15 @@ elif userchoice == "P" or userchoice == "p":
 elif userchoice == "S" or userchoice == "s":
     print("You selected...Scissors")
 else:
+    # Error catch for incorrect inputs:
     print("Incorrect selection")
     print("Try again next time when you know how to read.")
+    # Stop game from running:
     play = False
 
+# Game run loop:
 if play == True:
+    # Game response for computer vs. user choice:
     if compchoice == "R":
         if userchoice == "R" or userchoice == "r":
             print("It is a draw")
@@ -44,7 +58,8 @@ if play == True:
     if compchoice == "S":
         if userchoice == "R" or userchoice == "r":
             print("Rock blunts scissors, you win")
-        elif userchoice == "P"  or userchoice == "p":
+        elif userchoice == "P" or userchoice == "p":
             print("Scissors cuts paper, I win")
         else:
             print("It is a draw")
+# Exit loop
