f0e4f1bc7419361784eb3375546b502085617afc
DaFalconHunter <123alexmbale@gmail.com>
2022-12-05 20:00:25 +0000

    feat: debug errors

--- python-yr-9-2022/debugging/rock-paper-scissor.py
+++ python-yr-9-2022/debugging/rock-paper-scissor.py
@@ -1,47 +1,50 @@
 import random
-compchoice=random.choice(["R","P","S"])
+compchoice = random.choice(["R","P","S"])
 print("Pick one of the following")
 print()
-print("(R) Rock")
-print("(P) Paper")
-print("(S) Scissors")
-userchoice=input("Enter R, P or S: ")
+print("(R or r) Rock")
+print("(P or p) Paper")
+print("(S or s) Scissors")
+userchoice = input("Enter R, P or S or r, p, s alternatively: ")
+
 play = True
-if compchoice = "R":
+
+if compchoice == "R":
     print("I selected...Rock")
 elif compchoice == "P":
     print("I selected...Paper")
 else:
     print("I selected...Scissors")
-if userchoice == "R":
+if userchoice == "R" or userchoice == "r":
     print("You selected...Rock")
-elif userchoice == "P":
+elif userchoice == "P" or userchoice == "p":
     print("You selected...Paper")
-elif userchoice == "S":
+elif userchoice == "S" or userchoice == "s":
     print("You selected...Scissors")
 else:
     print("Incorrect selection")
-play = False
+    print("Try again next time when you know how to read.")
+    play = False
 
 if play == True:
     if compchoice == "R":
-        if userchoice == "R":
+        if userchoice == "R" or userchoice == "r":
             print("It is a draw")
-        elif userchoice == "P":
+        elif userchoice == "P" or userchoice == "p":
             print("Paper wraps rock, you win")
         else:
             print("Rock blunts scissors, I win")
     if compchoice == "P":
-        if userchoice == "R":
-           print("Paper wraps rock, I win")
-        elif userchoice == "P":
+        if userchoice == "R" or userchoice == "r":
+            print("Paper wraps rock, I win")
+        elif userchoice == "P" or userchoice == "p":
             print("It is a draw")
         else:
             print("Scissors cuts paper, you win")
     if compchoice == "S":
-        if userchoice == "R":
-            print("Rock blunts scissors, I win")
-        elif userchoice == "P":
+        if userchoice == "R" or userchoice == "r":
+            print("Rock blunts scissors, you win")
+        elif userchoice == "P"  or userchoice == "p":
             print("Scissors cuts paper, I win")
         else:
-            print("It is a drawn")
+            print("It is a draw")
