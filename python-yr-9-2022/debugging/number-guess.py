20cf8a3518abd282f475f984d49653e40ab67737
DaFalconHunter <123alexmbale@gmail.com>
2022-12-08 11:46:03 +0000

    refactor: simplify guesses variable incrementation

--- python-yr-9-2022/debugging/number-guess.py
+++ python-yr-9-2022/debugging/number-guess.py
@@ -4,14 +4,12 @@ playagain = True
 guesses = 0
 while playagain == True:
     usernum = int(input("Guess a number:" ))
+    guesses += 1
     if usernum == compnum:
         print("Correct")
         playagain = False
-        guesses += 1
     elif usernum > compnum:
         print("Too high")
-        guesses += 1
     else:
         print("Too low")
-        guesses += 1
 print("You took", guesses, "turns to get it right")
