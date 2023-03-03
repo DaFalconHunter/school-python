ef16f13a64c2989b8e1466ba5f71a35a57b6beb6
DaFalconHunter <92971251+DaFalconHunter@users.noreply.github.com>
2023-03-03 09:29:03 +0000

    fix: fix same inputs logic error to "is the same size as"

--- python-yr-9-2022/debugging/number-size.py
+++ python-yr-9-2022/debugging/number-size.py
@@ -2,5 +2,7 @@ num1 = int(input("Enter a number: "))
 num2 = int(input("Enter another number: "))
 if num1 > num2:
     print(num1, "is bigger than", num2)
+elif num1 == num2:
+    print(num1, "is the same size as", num2)
 else:
     print(num1, "is smaller than", num2)
