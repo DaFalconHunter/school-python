401af889c2c9e64c236a68839f024911fe3a592b
DaFalconHunter <92971251+DaFalconHunter@users.noreply.github.com>
2023-03-30 11:35:37 +0000

    feat: complete holiday eval  - use conditionals to calculate holidays for part time employees

--- python-yr-9-2022/40-algorithm-challenge-responses/difficulty-4/challenge-13.py
+++ python-yr-9-2022/40-algorithm-challenge-responses/difficulty-4/challenge-13.py
@@ -9,9 +9,17 @@ The company gives each employees 28 days holiday each year. Holidays are awarded
 
 2. Part time employees get a proportion of holiday allowance based on how many days they work, e.g. An employee who works 1 day a week would only get 1/5th of the holidays allowed.
 """
+import math
 
 # Retrieve employee details:
 
 # Full time or part time?
 emp_type = input('Full time (f) or part time (p)? ')
-print(emp_type)
+
+if emp_type == 'f':
+    print("As a full time employee who works 5 days a week, you'll be allowed 28 days of holiday a year.")
+else:
+    pt_emp_work_days = int(input('Please enter the no. of days you work per year: '))
+    pt_emp_work_holidays = math.floor(pt_emp_work_days / 5)
+    print(f"As a part time employee who works {pt_emp_work_days} days per year, your allowance of holidays will be {pt_emp_work_holidays} days.")
+
