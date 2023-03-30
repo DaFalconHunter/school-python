93be8e7bf9db2e085319b6b1344494bfd811b5d8
DaFalconHunter <92971251+DaFalconHunter@users.noreply.github.com>
2023-03-30 11:16:00 +0000

    feat: write challenge tasks

---
+++ python-yr-9-2022/40-algorithm-challenge-responses/difficulty-4/challenge-13.py
@@ -0,0 +1,11 @@
+"""
+Challenge 13:
+
+A company calculates holiday allowance for employees.
+
+The company gives each employees 28 days holiday each year. Holidays are awarded based on the following rules:
+
+1. Full time employees who work 5 days a week get 28 days holiday a year
+
+2. Part time employees get a proportion of holiday allowance based on how many days they work, e.g. An employee who works 1 day a week would only get 1/5th of the holidays allowed.
+"""
