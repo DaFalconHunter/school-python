f58b3d3637cc5219824401f45322ed5d7990080a
DaFalconHunter <92971251+DaFalconHunter@users.noreply.github.com>
2023-07-13 11:05:56 +0000

    feat: initialise traffic light colours

--- python-yr-9-2022/scenarios/pedestrian_crossing.py
+++ python-yr-9-2022/scenarios/pedestrian_crossing.py
@@ -1,4 +1,9 @@
 red_man = "RED MAN 🔴 🚶"
 green_man = "GREEN MAN 🟢 🚶"
 
+red_light = "RED LIGHT 🔴 🚦"
+amber_light = "AMBER LIGHT 🟠 🚦"
+green_light = "GREEN LIGHT 🟢 🚦"
+
 man_colour = red_man
+traffic_light_colour = "GREEN LIGHT 🟢 🚦"
