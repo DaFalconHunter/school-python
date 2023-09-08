4384271c55047c01f58a29395860e533c9da6531
DaFalconHunter <92971251+DaFalconHunter@users.noreply.github.com>
2023-09-08 08:50:16 +0000

    feat: solution

---
+++ python-yr-9-2022/Selection Statements Sep 2023 Y10 Cycle 1/solution_6.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+
+if num1 % 2 != 0 and num2 % 2 != 0:
+    print("Both these numbers are odd")
+else:
+    print("These numbers aren't both odd")
