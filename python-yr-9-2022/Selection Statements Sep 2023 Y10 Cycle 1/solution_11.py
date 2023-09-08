85d1dfcd98d97e119e09baa3b0643cfdf740ded8
DaFalconHunter <92971251+DaFalconHunter@users.noreply.github.com>
2023-09-08 09:13:18 +0000

    feat: solution

---
+++ python-yr-9-2022/Selection Statements Sep 2023 Y10 Cycle 1/solution_11.py
@@ -0,0 +1,6 @@
+num = int(input("Enter a number: "))
+
+if num >= 100 and num <= 999 or num <= -100 and num >= -999:
+    print("This number is a three-digit number")
+else:
+    print("This number is not a three-digit number")
