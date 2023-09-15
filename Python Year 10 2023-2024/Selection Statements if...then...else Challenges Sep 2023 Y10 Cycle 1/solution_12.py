7669675bf294406ac55cd9d8014546fa7186cf88
DaFalconHunter <92971251+DaFalconHunter@users.noreply.github.com>
2023-09-15 08:34:37 +0000

    style: format document

--- Python Year 10 2023-2024/Selection Statements if...then...else Challenges Sep 2023 Y10 Cycle 1/solution_12.py
+++ Python Year 10 2023-2024/Selection Statements if...then...else Challenges Sep 2023 Y10 Cycle 1/solution_12.py
@@ -1,9 +1,7 @@
-num1 = input("Enter the first number: ") 
-
-num2 = input("Enter the second number: ") 
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
 
 if num1 > num2:
-    print(f"{num2} is smaller than {num1}.") 
+    print(f"{num2} is smaller than {num1}.")
 else:
-    print(f"{num1} is smaller than {num2}.") 
- 
+    print(f"{num1} is smaller than {num2}.")
