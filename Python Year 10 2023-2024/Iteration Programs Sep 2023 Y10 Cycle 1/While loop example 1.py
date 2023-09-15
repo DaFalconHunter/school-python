9d64fea4a78d5fef0305ebf3c0e577e2fa5efc8c
DaFalconHunter <92971251+DaFalconHunter@users.noreply.github.com>
2023-09-15 09:24:59 +0000

    feat: initial commit

---
+++ Python Year 10 2023-2024/Iteration Programs Sep 2023 Y10 Cycle 1/While loop example 1.py
@@ -0,0 +1,7 @@
+#While loop example 1
+#Annotate what each line is doing
+#Copy and paste the annotated code into your programming notes document
+again="yes"
+while again=="yes":
+    print("Hello")
+    again=input("Do you want to loop again? ")
