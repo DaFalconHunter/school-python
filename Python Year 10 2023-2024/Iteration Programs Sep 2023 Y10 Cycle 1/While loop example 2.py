8295168e4f85dd853d83b3ba88f0670894414587
DaFalconHunter <92971251+DaFalconHunter@users.noreply.github.com>
2023-09-15 09:30:47 +0000

    feat: annotate code

--- Python Year 10 2023-2024/Iteration Programs Sep 2023 Y10 Cycle 1/While loop example 2.py
+++ Python Year 10 2023-2024/Iteration Programs Sep 2023 Y10 Cycle 1/While loop example 2.py
@@ -1,8 +1,14 @@
 # While loop example 2
 # Annotate what each line is doing
 # Copy and paste the annotated code into your programming notes document
+
+# Set total to value of 0
 total = 0
+# Loop while total is less than 100
 while total < 100:
+    # Request integer number user input:
     num = int(input("Enter a number: "))
+    # Add number to total
     total = total + num
+    # Output total
     print("The total is", total)
