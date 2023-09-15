ab4f5240a9d107e27c40a3beb50421344e2ed3a7
DaFalconHunter <92971251+DaFalconHunter@users.noreply.github.com>
2023-09-15 09:26:23 +0000

    feat: annotate code

--- Python Year 10 2023-2024/Iteration Programs Sep 2023 Y10 Cycle 1/While loop example 1.py
+++ Python Year 10 2023-2024/Iteration Programs Sep 2023 Y10 Cycle 1/While loop example 1.py
@@ -1,7 +1,13 @@
 # While loop example 1
 # Annotate what each line is doing
 # Copy and paste the annotated code into your programming notes document
+
+# Assign value of yes to var again:
 again = "yes"
+
+# Loop while again has a value of "yes"
 while again == "yes":
+    # Print hello
     print("Hello")
+    # Ask whether to loop again
     again = input("Do you want to loop again? ")
