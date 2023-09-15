9b2181945005256b5720307f90f3fe6f978d7c6b
DaFalconHunter <92971251+DaFalconHunter@users.noreply.github.com>
2023-09-15 09:27:15 +0000

    feat: initial commit

---
+++ Python Year 10 2023-2024/Iteration Programs Sep 2023 Y10 Cycle 1/While loop example 2.py
@@ -0,0 +1,9 @@
+#While loop example 2
+#Annotate what each line is doing
+#Copy and paste the annotated code into your programming notes document
+total = 0
+while total<100:
+    num=int(input("Enter a number: "))
+    total=total + num
+    print("The total is", total)
+
