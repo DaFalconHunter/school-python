ccebbfd40927f1a447e44d07c991db0fb824152f
DaFalconHunter <92971251+DaFalconHunter@users.noreply.github.com>
2023-09-22 08:07:01 +0000

    feat: annotate & format code

--- Do Nows/do_now_loop_&_if_22_09_23.py
+++ Do Nows/do_now_loop_&_if_22_09_23.py
@@ -1,6 +1,9 @@
+# Initialise question var
 question_1 = ""
+# Initialise loop var to True
 carry_on = True
 while carry_on == True:
+    # Establish game menu
     print("Quiz - Main Menu")
     print(
         """
@@ -9,7 +12,9 @@ while carry_on == True:
     3. Quiz
     """
     )
+    # Ask for user choice
     choice = input("Choose option (Enter 1 or 2 or 3): ")
+    # Selection for instructions, question or exit
     if choice == "1":
         print("Instructions: Read the question and enter your answer.")
     elif choice == "2":
@@ -18,9 +23,11 @@ while carry_on == True:
         if question_1 == "Paris":
             print("Correct")
         else:
-            print("Inorrect")
+            print("Incorrect")
     elif choice == "3":
         print("Thanks for playing")
+        # Setting loop variable to False kills the loop
         carry_on = False
+    # Invalid input catching
     elif choice != "":
         print("Not a valid choice.  Enter 1 or 2 or 3: ")
