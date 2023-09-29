a893c5ba9b001cac5f5950961558efcae96f1b7f
DaFalconHunter <92971251+DaFalconHunter@users.noreply.github.com>
2023-09-29 09:25:03 +0000

    feat: initial commit

---
+++ Python Year 10 2023-2024/Time2Code.today/Python Level 4/dungeon_master_1*.py
@@ -0,0 +1,20 @@
+# Dungeon master program
+
+# -------------------------
+# Import libraries
+# -------------------------
+
+
+# -------------------------
+# Subprograms
+# -------------------------
+# Function to return if a skill check is passed in DnD5e
+def check_skill(skill, modifier, dm_value):
+
+
+# -------------------------
+# Main program
+# -------------------------
+
+result = check_skill(skill, modifier, dm_value)
+print(result)
