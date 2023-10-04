48307961133a1dd35c4a14c64df6e0b473d34060
DaFalconHunter <92971251+DaFalconHunter@users.noreply.github.com>
2023-10-04 11:37:02 +0000

    fix: fix redefinition of check_skill params from outer scope

--- Python Year 10 2023-2024/Time2Code.today/Python Level 4/dungeon_master_1*.py
+++ Python Year 10 2023-2024/Time2Code.today/Python Level 4/dungeon_master_1*.py
@@ -9,18 +9,18 @@ import random
 # Subprograms
 # -------------------------
 # Function to return if a skill check is passed in DnD5e
-def check_skill(skill, modifier, dm_value):
+def check_skill(skill_input, modifier_input, dm_value_input):
     roll = random.randint(1, 20)
-    if (skill + modifier) >= (dm_value + 5):
+    if (skill_input + modifier_input) >= (dm_value_input + 5):
         return "Automatic pass"   
     print(f"You rolled a {roll}")
     if roll == 1:
         return "Critical fail"
     elif roll == 20:
         return "Critical success"
-    elif (skill + roll + modifier) >= dm_value:
+    elif (skill_input + roll + modifier_input) >= dm_value_input:
         return "Check passed"
-    elif (skill + roll + modifier) < dm_value:
+    elif (skill_input + roll + modifier_input) < dm_value_input:
         return "Check failed"
 
 # -------------------------
