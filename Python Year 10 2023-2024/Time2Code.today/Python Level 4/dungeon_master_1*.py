75ed4bead1ba04706d95ae9a700ca4a33d59e8b5
DaFalconHunter <92971251+DaFalconHunter@users.noreply.github.com>
2023-10-04 11:33:13 +0000

    feat: solution

--- Python Year 10 2023-2024/Time2Code.today/Python Level 4/dungeon_master_1*.py
+++ Python Year 10 2023-2024/Time2Code.today/Python Level 4/dungeon_master_1*.py
@@ -3,18 +3,33 @@
 # -------------------------
 # Import libraries
 # -------------------------
-
+import random
 
 # -------------------------
 # Subprograms
 # -------------------------
 # Function to return if a skill check is passed in DnD5e
 def check_skill(skill, modifier, dm_value):
-
+    roll = random.randint(1, 20)
+    if (skill + modifier) >= (dm_value + 5):
+        return "Automatic pass"   
+    print(f"You rolled a {roll}")
+    if roll == 1:
+        return "Critical fail"
+    elif roll == 20:
+        return "Critical success"
+    elif (skill + roll + modifier) >= dm_value:
+        return "Check passed"
+    elif (skill + roll + modifier) < dm_value:
+        return "Check failed"
 
 # -------------------------
 # Main program
 # -------------------------
+
+skill = int(input("Enter the skill value: "))
+modifier = int(input("Enter any modifiers: "))
+dm_value = int(input("Enter the Dungeon Master's pass number: "))
 
 result = check_skill(skill, modifier, dm_value)
 print(result)
