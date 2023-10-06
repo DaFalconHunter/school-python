1674d467c7c7c69915bc827f9c66ac2daffcb41f
DaFalconHunter <92971251+DaFalconHunter@users.noreply.github.com>
2023-10-06 08:32:55 +0000

    feat: initial commit + boilerplate code

---
+++ Python Year 10 2023-2024/Time2Code.today/Python Level 4/seconds_in_a_day_2*.py
@@ -0,0 +1,22 @@
+# Seconds in a day program
+
+# -------------------------
+# Subprograms
+# -------------------------
+def seconds_to_hours(seconds):
+
+
+
+def seconds_to_minutes(seconds):
+
+
+
+def seconds_remaining(seconds):
+
+
+
+# -------------------------
+# Main program
+# -------------------------
+
+print(hours, "hours", minutes, "minutes", seconds, "seconds")
