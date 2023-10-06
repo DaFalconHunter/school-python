29990ac7719a1d0e8b03a19a2e8f77df44b94a80
DaFalconHunter <92971251+DaFalconHunter@users.noreply.github.com>
2023-10-06 08:17:26 +0000

    feat: add boilerplate code

---
+++ Python Year 10 2023-2024/Time2Code.today/Python Level 4/guess_the_number_2*.py
@@ -0,0 +1,16 @@
+# Guess the number program
+
+# -------------------------
+# Import libraries
+# -------------------------
+
+
+# -------------------------
+# Subprograms
+# -------------------------
+def play_guess_the_number(min, max):
+
+
+# -------------------------
+# Main program
+# -------------------------
