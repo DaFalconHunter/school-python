345a652994afdec57018a3616ea245de9daaecba
DaFalconHunter <92971251+DaFalconHunter@users.noreply.github.com>
2023-10-06 08:30:02 +0000

    feat: solution

--- Python Year 10 2023-2024/Time2Code.today/Python Level 4/guess_the_number_2*.py
+++ Python Year 10 2023-2024/Time2Code.today/Python Level 4/guess_the_number_2*.py
@@ -3,14 +3,31 @@
 # -------------------------
 # Import libraries
 # -------------------------
-
+import random
 
 # -------------------------
 # Subprograms
 # -------------------------
 def play_guess_the_number(min, max):
-
+    random_num = random.randint(min, max)
+    guess = int(input("Guess the number: "))
+    num_guesses = 1
+    while guess != random_num:        
+        if random_num >= guess:
+            print("Your guess was too low")
+            num_guesses += 1
+            guess = int(input("Guess the number: "))
+        elif random_num <= guess:
+            print("Your guess was too high")
+            num_guesses += 1
+            guess = int(input("Guess the number: "))
+        elif random_num == guess:
+            break
+    if random_num == guess:
+        print(f"It took you {num_guesses} guesses.")
+        print("You guessed correctly!")
 
 # -------------------------
 # Main program
 # -------------------------
+play_guess_the_number(1, 100)
