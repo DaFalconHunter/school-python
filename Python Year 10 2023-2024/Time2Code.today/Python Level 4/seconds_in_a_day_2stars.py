53b9f9b059c4f6c9ee2f7271b4ddfd4360e96542
DaFalconHunter <123alexmbale@gmail.com>
2023-12-01 10:39:41 +0000

    feat: fill out function bases

--- Python Year 10 2023-2024/Time2Code.today/Python Level 4/seconds_in_a_day_2stars.py
+++ Python Year 10 2023-2024/Time2Code.today/Python Level 4/seconds_in_a_day_2stars.py
@@ -4,15 +4,15 @@
 # Subprograms
 # -------------------------
 def seconds_to_hours(seconds):
-
+    return seconds / 3600
 
 
 def seconds_to_minutes(seconds):
-
+    return seconds / 60 % 60
 
 
 def seconds_remaining(seconds):
-
+    return seconds % 60
 
 
 # -------------------------
