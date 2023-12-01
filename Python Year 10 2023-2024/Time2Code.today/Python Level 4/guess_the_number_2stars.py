c06b51d56d21d583f26fb65f143d418278d99101
DaFalconHunter <123alexmbale@gmail.com>
2023-12-01 10:35:41 +0000

    chore: specify in input prompts the guess range

--- Python Year 10 2023-2024/Time2Code.today/Python Level 4/guess_the_number_2stars.py
+++ Python Year 10 2023-2024/Time2Code.today/Python Level 4/guess_the_number_2stars.py
@@ -10,17 +10,17 @@ import random
 # -------------------------
 def play_guess_the_number(minimum, maximum):
     random_num = random.randint(minimum, maximum)
-    guess = int(input("Guess the number: "))
+    guess = int(input("Guess the number between 1 & 100: "))
     num_guesses = 1
     while guess != random_num:        
         if random_num >= guess:
             print("Your guess was too low")
             num_guesses += 1
-            guess = int(input("Guess the number: "))
+            guess = int(input("Guess the number between 1 & 100: "))
         elif random_num <= guess:
             print("Your guess was too high")
             num_guesses += 1
-            guess = int(input("Guess the number: "))
+            guess = int(input("Guess the number between 1 & 100: "))
         elif random_num == guess:
             break
     if random_num == guess:
