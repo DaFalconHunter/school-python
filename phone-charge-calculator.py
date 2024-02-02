bdab5a74994c37c80f9eb58bec658314cb79fb56
DaFalconHunter <92971251+DaFalconHunter@users.noreply.github.com>
2024-02-02 09:15:20 +0000

    chore: remove files from main file tree

--- phone-charge-calculator.py
+++
@@ -1,9 +0,0 @@
-monthly_tariff = 23.00
-per_min_rate = 0.02
-total_charge = monthly_tariff
-mins_used = int(input("Enter the no. of minutes used: "))
-if mins_used > 600:
-    extra_mins = mins_used - 600
-    extra_charge = extra_mins * per_min_rate
-    total_charge += extra_charge
-print(f"£{total_charge}")
