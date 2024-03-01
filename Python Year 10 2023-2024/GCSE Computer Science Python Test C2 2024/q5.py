387dfb44d7ad638157ab747e5bea2d8293ea8cb8
DaFalconHunter <92971251+DaFalconHunter@users.noreply.github.com>
2024-03-01 10:36:43 +0000

    feat: implement solution

--- Python Year 10 2023-2024/GCSE Computer Science Python Test C2 2024/q5.py
+++ Python Year 10 2023-2024/GCSE Computer Science Python Test C2 2024/q5.py
@@ -3,3 +3,28 @@
 #The user then inputs which arithmetic operator they want
 #to use to perform a calculation between the two numbers, +  -  /  *.
 #The calculation is then performed and the output printed.
+
+import random
+
+num1 = random.randint(1, 100)
+num2 = random.randint(1, 100)
+print(f"Your numbers: no.1 = {num1}; no.2 = {num2}")
+operator = input("Enter the operation I will conduct (+, -, *, /): ")
+while True:
+    match operator:
+        case "+":
+            calc = num1 + num2
+            break
+        case "-":
+            calc = num1 - num2
+            break
+        case "*":
+            calc = num1 * num2
+            break
+        case "/":
+            calc = num1 / num2
+            break
+        case _:
+            operator = input("Please enter your desired operator: ")
+
+print(calc)
