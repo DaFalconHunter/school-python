9dca9dcdf07e30d6d88e4ccf124d7de56ac594b3
DaFalconHunter <92971251+DaFalconHunter@users.noreply.github.com>
2024-05-24 09:49:39 +0000

    feat: upload all changes from local file to be explained in later commits

--- Extended Programming Challenges/music_quiz_game.py
+++ Extended Programming Challenges/music_quiz_game.py
@@ -17,10 +17,10 @@ Where appropriate, input from the user should be validated.
 Design, write, test and refine a system that:
 1.	✔ Allows a player to enter their details, which are then authenticated to ensure that they are an authorised player.
 2.	✔ Stores a list of song names and artists in an external file.
-3.	Selects a song from the file, displaying the artist and the first letter of each word of the song title.
-4.	Allows the user up to two chances to guess the name of the song, stopping the game if they guess a song incorrectly on the second chance.
-5.	If the guess is correct, add the points to the player's score depending on the number of guesses.
-6.	Displays the number of points the player has when the game ends.
+3.	✔ Selects a song from the file, displaying the artist and the first letter of each word of the song title.
+4.	✔ Allows the user up to two chances to guess the name of the song, stopping the game if they guess a song incorrectly on the second chance.
+5.	✔ If the guess is correct, add the points to the player's score depending on the number of guesses.
+6.	✔ Displays the number of points the player has when the game ends.
 7.	Stores the name of the player and their score in an external file.
 8.	Displays the score and player name of the top 5 winning scores from the external file.
 
@@ -29,9 +29,7 @@ Design, write, test and refine a system that:
 import music_quiz_game_songs
 
 player_username = "Bob"
-player_password = "468Th@nks-f0r_theF1sh"
-username_input = 0
-password_input = 0
+player_password = "42Th@nks-f0r_theF1sh"
 
 """
 while True:
@@ -44,21 +42,59 @@ while True:
         print("Incorrect username or password")
 """
 
+
 songs_list = music_quiz_game_songs.songs_and_artists
+# print(songs_list)
+questions = []
 
 for i in range(len(songs_list)):
     # Print artist:
     # print(songs_list[i][0] + f" {songs_list[i][1][0]}")
     artist = songs_list[i][0]
     # print(artist)
-    song_split = songs_list[i][1].split()
+    song_split = songs_list[i][1]
     # print(song_split)
-    song_split_1st_letters = [s[0] for s in songs_list[i][1].split()]
+    song_split_1st_letters = [ s[0] for s in songs_list[i][1].split() ]
     song_join = "    ".join(song_split_1st_letters)
-    print(f"{artist}   {song_join}")
-    # song_split_1st_letters =
+    questions.extend([[f"{artist}: ", f"{song_join}    "]])
+    # print(f"{artist}   {song_join}    ")
+    # song_split_1st_letters = 
     # song_1st_letters = songs_list[i][1].split()[0]
     # print(artist, song_1st_letters)
 
     # for j in range(len(songs_list[i])):
-    # print(songs_list[i][j])
+        # print(songs_list[i][j])
+
+# print(questions)
+points = 0
+for i in range(len(questions)):
+    print(questions[i][0] + questions[i][1])
+    chances = 2
+    while chances > 0:
+        song_name = "".join(songs_list[i][1].lower())
+        # print(song_name)
+        response = input("Enter the artist's song: ").lower()
+        response = "".join(response)
+        # print(response)
+        if response == song_name:
+            print("Well done!")
+            if chances == 2:
+                points += 3
+                print("You earned 3 points!")
+            elif chances == 1:
+                points += 1 
+                print("You earned 1 point.")
+            break
+        else:
+            print("Uncultured fool, ya failiyer!")
+            chances -= 1
+    if chances == 0:
+        print("Get cultured, dood")
+        break
+
+print(f"Your score: {points} points")
+
+player_scores = open("player_scores.txt", "a")
+player_scores.write(f"{username_input}: {points}")
+player_scores = open("player_scores.txt", "r")
+print(player_scores.read())
