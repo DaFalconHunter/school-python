54dbdf831321528e6bed8c4801f79205a1e4811e
DaFalconHunter <92971251+DaFalconHunter@users.noreply.github.com>
2024-06-14 09:19:02 +0000

    feat: solution

---
+++ Do Nows/do-now-14-06-24.py
@@ -0,0 +1,10 @@
+first = "Computer Science"
+second = "is great"
+
+print(len(first))
+print(len(second) // 3)
+print(3 ** 2)
+
+print(second[3:8])
+print(first[0:8])
+print(f"{first[9:16]} {second}")
