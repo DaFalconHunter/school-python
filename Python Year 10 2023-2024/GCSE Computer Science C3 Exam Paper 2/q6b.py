9f8a62e410c8e0d0eb3e8293cb087acff20e2021
DaFalconHunter <92971251+DaFalconHunter@users.noreply.github.com>
2024-06-26 11:41:07 +0000

    feat: initial commit

---
+++ Python Year 10 2023-2024/GCSE Computer Science C3 Exam Paper 2/q6b.py
@@ -0,0 +1,16 @@
+# Question 6b
+# Complete line C to generate a 4 digit number between 1000 and 9999
+# Line 6 has two syntax errors, correct the code.
+# Use .upper to make the program robust.
+# Upload your completed solution to Teams.
+
+import random
+a = input("Enter first letter of first name") 
+b = input("Enter first letter of second name") 
+c = 
+again = “Y”
+while again = "Y"
+    pilotCode = a + b + str(c)
+    print(pilotCode)
+    again=input(“Do you want to generate another code? “
+print(“Goodbye”)
