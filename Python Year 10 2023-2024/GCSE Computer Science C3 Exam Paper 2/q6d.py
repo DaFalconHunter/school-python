5d41c88fd0b5cd5cd9fb66246e695894b6efdc0f
DaFalconHunter <92971251+DaFalconHunter@users.noreply.github.com>
2024-06-26 11:50:29 +0000

    feat: solution

--- Python Year 10 2023-2024/GCSE Computer Science C3 Exam Paper 2/q6d.py
+++ Python Year 10 2023-2024/GCSE Computer Science C3 Exam Paper 2/q6d.py
@@ -2,3 +2,21 @@
 # See the question on page 10 of the cycle 3 assessment.
 # Program a solution to the problem.
 # Upload your completed solution to Teams.
+
+flxp = int(input("Enter your no. of years of flying experience: "))
+miles = int(input("Enter the no. of miles you've flown today: "))
+payperday = 0
+paypermile = 0
+
+if flxp < 2:
+    payperday = 120
+    paypermile = 0.45
+elif flxp >= 2 and flxp <= 5:
+    payperday = 150
+    paypermile = 0.65
+elif flxp > 5:
+    payperday = 180
+    paypermile = 0.85
+    
+pay = payperday + (paypermile * miles)
+print(f"Your pay today is £{pay}0")
