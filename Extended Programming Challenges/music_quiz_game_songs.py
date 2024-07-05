d5f1057448c4ce82ce0ac08ab47d806a59859dc0
DaFalconHunter <92971251+DaFalconHunter@users.noreply.github.com>
2024-07-05 09:48:00 +0000

    feat: add one song to the list

--- Extended Programming Challenges/music_quiz_game_songs.py
+++ Extended Programming Challenges/music_quiz_game_songs.py
@@ -3,5 +3,6 @@ songs_and_artists = [
     ["Imagine Dragons", "Natural"],
     ["Sia", "Cheap Thrills"],
     ["Felix Mendelssohn", "Violin Concerto in E Minor"],
+    ["Jean Sibelius", "Violin Concerto in D Minor"],
     ["Hans Zimmer", "Time from Inception"]
 ]
