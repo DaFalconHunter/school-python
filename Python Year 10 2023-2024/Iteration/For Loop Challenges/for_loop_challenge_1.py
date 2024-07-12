378d548b0fb776dedb5f1b8d6919f43f70713a4a
DaFalconHunter <92971251+DaFalconHunter@users.noreply.github.com>
2024-07-12 09:10:02 +0000

    style: format and comment code to show loop type

--- Python Year 10 2023-2024/Iteration/For Loop Challenges/for_loop_challenge_1.py
+++ Python Year 10 2023-2024/Iteration/For Loop Challenges/for_loop_challenge_1.py
@@ -1,8 +1,8 @@
-#For loop challenge 1
-
-#Rearrange the two lines of code below
-#to display the word "kebab" 10 times
-#HINT: There might be some indentation
+# For loop challenge 1
+# Count-controlled loop
+# Rearrange the two lines of code below
+# to display the word "kebab" 10 times
+# HINT: There might be some indentation
 
 for i in range(10):
     print("kebab")
