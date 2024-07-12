14d98f7077dc5dac5ba1009ec13dbc4c7f5b455f
DaFalconHunter <92971251+DaFalconHunter@users.noreply.github.com>
2024-07-12 09:15:32 +0000

    feat: ammend to include value of i

--- Python Year 10 2023-2024/Iteration/For Loop Challenges/for_loop_challenge_1.py
+++ Python Year 10 2023-2024/Iteration/For Loop Challenges/for_loop_challenge_1.py
@@ -4,5 +4,6 @@
 # to display the word "kebab" 10 times
 # HINT: There might be some indentation
 
-for i in range(10):
-    print("kebab")
+for i in range(20):
+    print("I is all there is", i)
+    # print(i)
