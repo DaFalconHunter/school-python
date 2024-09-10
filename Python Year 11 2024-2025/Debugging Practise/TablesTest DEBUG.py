71ed31964cb7afe4b2490457e7c25bd0f104b1a6
DaFalconHunter <92971251+DaFalconHunter@users.noreply.github.com>
2024-09-10 14:31:12 +0000

    feat: solution

--- Python Year 11 2024-2025/Debugging Practise/TablesTest DEBUG.py
+++ Python Year 11 2024-2025/Debugging Practise/TablesTest DEBUG.py
@@ -2,32 +2,34 @@
 # There are 5 errors, debug the program and test until fully working.
 
 import random
+
 print("Multiplication tables test")
 print("**********************\n")
 print("What table would you like to be tested on?")
-timestable=Int(input ())
+timestable = int(input(""))
 print("You will be given 5 questions")
 
 score = 0
 loopcounter = 1
-while loopcounter<=10:
-    number = random.randint(3, 12)    
-    print(timestable, "X", number,"?")
+while loopcounter <= 5:
+    number = random.randint(3, 12)
+    print(timestable, "X", number, "?")
     useranswer = int(input())
-    correctanswer = number - timestable
-    loopcounter+=1
+    correctanswer = number * timestable
+    loopcounter += 1
     if useranswer == correctanswer:
-        score+=1
+        score += 1
         print("Correct")
     else:
-        Print ("Wrong! The correct answer is",correctanswer)
+        print("Wrong! The correct answer is", correctanswer)
 
 if score == 5:
-    print("Congratulatios! You scored", score,"out of 5")
-elif score==4:
-    print ("Pretty good... you scored",score,"out of 5")
-elif score==3:
-    print ("Could do better... you scored",score,"out of 5")
+    print("Congratulatios! You scored", score, "out of 5")
+elif score == 4:
+    print("Pretty good... you scored", score, "out of 5")
+elif score == 3:
+    print("Could do better... you scored", score, "out of 5")
 else:
-    print "Oh dear! Back to the drawing board... you scored",score,"out of 5")
+    print("Oh dear! Back to the drawing board... you scored", score, "out of 5")
+
 input("\n\nPress ENTER to exit")
--- Python Year 11 2024-2025/Debugging Practise/Username progam DEBUG.py
+++ Python Year 11 2024-2025/Debugging Practise/Username progam DEBUG.py
@@ -1,13 +1,17 @@
 forename = input("Enter forename: ")
 surname = input("Enter surname: ")
+
+# Concatenates the first 4 characters of forename and surname together
 userp1 = forename[:4] + surname[:4]
 userp1 = userp1.lower()
 
+# Loop iterates from 1000 to 1002, incrementing by 1 on each iteration, and uses that number as an attachment to the auto-generated username
 for counter in range(1000,1003):
     userp2 = str(counter)
     print("Suggested username:",userp1 + userp2)
 
 username = ""
+# Loops so longs as the entered username is less than 4 chars or greater than 12 chars in length
 while len(username) < 4 or len(username) > 12:
     username = input("Enter a chosen username: ")
 
