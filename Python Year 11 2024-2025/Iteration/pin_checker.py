3f3f44e6dab96fd6e64452140f7b6b1d493548a3
DaFalconHunter <92971251+DaFalconHunter@users.noreply.github.com>
2024-10-04 13:29:04 +0000

    feat: solution

--- Python Year 11 2024-2025/Iteration/pin_checker.py
+++ Python Year 11 2024-2025/Iteration/pin_checker.py
@@ -1,8 +1,33 @@
 pin = '1977'
 
+# userPin = ""
+"""
 while True:
     userPin = input("Enter your 4-digit PIN: ")
 
     if userPin == pin:
         print("PIN correct, you may proceed.")
         break
+    else:
+        print("The PIN you entered was incorrect.\nPlease try again.")
+"""
+
+"""
+while pin != userPin:
+    userPin = input("Enter your 4-digit PIN: ")
+    if userPin == pin:
+        print("PIN correct, you may proceed.")
+    else:
+        print("The PIN you entered was incorrect.\nPlease try again.")
+"""
+
+attempt = 0
+for i in range(0, 3):
+    userPin = input("Enter your 4-digit PIN: ")
+    attempt += 1
+
+    if userPin == pin:
+        print("PIN correct, you may proceed.")
+        break
+else:
+    print("You are out of attempts.")
