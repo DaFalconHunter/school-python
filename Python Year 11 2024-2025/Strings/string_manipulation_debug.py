2ae8d3a31b765074c0bd9f84467cc8b012d10aca
DaFalconHunter <92971251+DaFalconHunter@users.noreply.github.com>
2024-10-18 13:05:20 +0000

    feat: solution

--- Python Year 11 2024-2025/Strings/string_manipulation_debug.py
+++ Python Year 11 2024-2025/Strings/string_manipulation_debug.py
@@ -1,35 +1,35 @@
 #Output initial and surname
 #De-bug the following program and follow the instructions
 
-firstName = input("Enter your first name: )
-surname = input(Enter your surname: ")
+firstName = input("Enter your first name: ")
+surname = input("Enter your surname: ")
 
 #What do you add at the end of this line to extract
 #the first character from the first name?
-initial = firstName[
+initial = firstName[0]
     
 #What do you add after the . to make sure the initial is uppercase?
-initialUpper = initial. ()
+initialUpper = initial.upper()
 
 surnameStart = surname[0]
 surnameStart = surnameStart.upper()
 
 # What do you need to add at the beginning of the surname
 #to find the length of the variable?
-length = (surname)
+length = len(surname)
 remain = surname[1:]
 
 lowerRemain = remain.lower()
 
 #What is the technique being used here to make sure the variables are strings?
-#ANSWER:
+#ANSWER: Casting
 surname = str(surnameStart) + str(lowerRemain)
 
 #What is the technique being used here to join two variables together?
-#ANSWER: 
+#ANSWER: String concatenation
 fullName = initialUpper + " " + surname
 print(fullName)
 
 #What is the purpose of the \n
-#ANSWER:
+#ANSWER: To create a new line
 input("\nPress ENTER to exit program ")
