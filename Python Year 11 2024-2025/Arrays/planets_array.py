530946b08b8040f40863088dcc1fd7acaf50eab6
DaFalconHunter <92971251+DaFalconHunter@users.noreply.github.com>
2024-12-17 14:55:04 +0000

    feat: solution

---
+++ Python Year 11 2024-2025/Arrays/planets_array.py
@@ -0,0 +1,4 @@
+planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+print(f"Six nearest planets to the Sun: {planets[:6]}")
+print(f"Three furthest planets from the Sun: {planets[-3:]}")
+print(f"All the planets: {planets}")
