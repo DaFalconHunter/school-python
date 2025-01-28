5fb0ab36b8a8d658256fdb7e99fb6a5eabe3d354
DaFalconHunter <92971251+DaFalconHunter@users.noreply.github.com>
2025-01-28 14:46:29 +0000

    feat: initial commit

---
+++ Python Year 11 2024-2025/Searching Algorithms/usernames_linear_search_debug.py
@@ -0,0 +1,17 @@
+# Username search
+# De-bug the following program
+# There are 8 syntax errors
+# How woudl you improve this program?
+usernames = "psmith", "ltorvalds" "pwatts", "sjones",
+             "mpatel", "bwright", "mgreen,
+             "dthomas", "nwhite", "fdavies"]
+
+search = iput("Type in username: ")
+
+for i in range(len(usernames))
+    if usernames[i] = search:
+    prit("Name found")
+
+
+
+
