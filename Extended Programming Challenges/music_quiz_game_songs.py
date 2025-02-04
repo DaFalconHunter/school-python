0785d3e0805a4692a7f3f0441852ecab817e509a
DaFalconHunter <92971251+DaFalconHunter@users.noreply.github.com>
2025-02-04 15:37:06 +0000

    feat: update songs

--- Extended Programming Challenges/music_quiz_game_songs.py
+++ Extended Programming Challenges/music_quiz_game_songs.py
@@ -5,10 +5,16 @@ songs_and_artists = [
     # ["Felix Mendelssohn", "Violin Concerto in E Minor"],
     # ["Jean Sibelius", "Violin Concerto in D Minor"],
     # ["Hans Zimmer", "Time from Inception"]
-    ["George Ezra", "Shotgun"],
-    ["Imagine Dragons", "Thunder"],
-    ["Sia", "Cheap Thrills"],
-    ["Klaus Badelt", "Pirates of the Caribbean"],
-    ["John Williams", "Hedwig's Theme"],
-    ["Hans Zimmer", "Time from Inception"]
+    # ["George Ezra", "Shotgun"],
+    # ["Imagine Dragons", "Thunder"],
+    # ["Sia", "Cheap Thrills"],
+    # ["Klaus Badelt", "Pirates of the Caribbean"],
+    # ["John Williams", "Hedwig's Theme"],
+    # ["Hans Zimmer", "Time from Inception"]
+    ["Glass Animals", "Heatwave",],
+    ["Dijit Dosanjh", "Do You Know"],
+    ["George Ezra", "Green Green Grass"],
+    ["Ed Sheeran", "Celestial"],
+    ["Barbatuques", "Baiana"],
+    ["Rick Astley", "Never Gonna Give You Up"]
 ]
