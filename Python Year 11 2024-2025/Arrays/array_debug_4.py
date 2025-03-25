08a7baeeca5a5c85b9b56a23607ff9726e178c44
DaFalconHunter <92971251+DaFalconHunter@users.noreply.github.com>
2025-03-25 14:22:41 +0000

    feat: solution

---
+++ Python Year 11 2024-2025/Arrays/array_debug_4.py
@@ -0,0 +1,12 @@
+#Create a list of numbers
+
+numbers = []
+more_numbers = True
+while more_numbers == True:
+    new_number = int(input("Number "))
+    numbers.append(new_number)
+    more = input("Do you want to add another number? (y/n) ")
+    if more == "n":
+        more_numbers = False
+        
+print(numbers)
