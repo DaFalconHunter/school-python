c369897482c08763273973985b980c1491eeea1f
DaFalconHunter <92971251+DaFalconHunter@users.noreply.github.com>
2025-05-06 13:08:13 +0000

    fix: correct syntax errors and improve file handling in challenge_109_debug.py

--- Python Year 11 2024-2025/Debugging Practise/challenge_109_debug.py
+++ Python Year 11 2024-2025/Debugging Practise/challenge_109_debug.py
@@ -1,27 +1,29 @@
-#Challenge 109
-#Debug the following program.
-#There are 5 syntax errors.
-#Comment on the code to explain what each section is doing
+""" Challenge 109"""
+# Debug the following program.
+# There are 5 syntax errors.
+# Comment on the code to explain what each section is doing
 
+# Allows the user to create a file, display the file, or add a new item to the file with user input.
 print("Choose from the following options:")
 print("1) Create new file")
 print("2) Display the file")
-print("3) Add a new item to the file"
-selection=input("Make a selection 1, 2, or 3: ")
+print("3) Add a new item to the file")
+selection = input("Make a selection 1, 2, or 3: ")
 
-if seletion=="1":
-    file=opn("Subject.txt","w")
-    subject=input("Please enter a subject: ")
+# Conduct file operations based on user selection
+if selection == "1":
+    file = open("Subject.txt", "w", encoding="utf-8")
+    subject = input("Please enter a subject: ")
     file.write(subject)
-elif selection="2":
-    file=open("Subject.txt","r")
-    contents=file.read()
+elif selection == "2":
+    file = open("Subject.txt", "r", encoding="utf-8")
+    contents = file.read()
     print(contents)
-elif selection=="3":
-    subject=input("Please enter a new subject: ")
-    file.wrte(subject)
-    file=open("Subject.txt","r")
-    contents=file.read
+elif selection == "3":
+    subject = input("Please enter a new subject: ")
+    file = open("Subject.txt", "r", encoding="utf-8")
+    file.write(subject)
+    contents = file.read()
     print(contents)
 else:
     print("Sorry you didn't type in 1, 2 or 3")
